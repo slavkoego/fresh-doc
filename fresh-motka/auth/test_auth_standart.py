d2101e54c01a8a35d159a04f6cdeed1c2d4bae37
Salavat Anamov <slavkoego@gmail.com>
2020-05-13 12:04:21 +0500

    added logout

--- fresh-motka/auth/test_auth_standart.py
+++ fresh-motka/auth/test_auth_standart.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
+import time
+import pytest
 
 link = "http://www.motka-freshdoc.qds.ru"
 browser = webdriver.Chrome()
 browser.get(link)
 input1 = browser.find_element_by_css_selector("div.header--main__wrap .qd__navigation__item .button-login")
 input1.click()
-
+time.sleep(1)
 login = browser.find_element_by_css_selector(".qd-overlay-auth__input:nth-child(1)")
 login.send_keys("test.qds.777@bk.ru")
 password = browser.find_element_by_css_selector(".qd-overlay-auth__input:nth-child(2)")
 password.send_keys("AnSa1991")
 browser.find_element_by_css_selector(".qd-overlay-auth__form .qd-overlay-auth__button").click()
-
-
+time.sleep(1)
+profile = browser.find_element_by_css_selector(" button.DD-button.qd-button_auth.DD-button__action-target > span.DD-button__caption")
+profile.click()
+logout = browser.find_element_by_css_selector("div.qd-auth-buttons-table.main-controls > button").click()
 browser.quit()
