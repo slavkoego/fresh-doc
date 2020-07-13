d891aecf183f849186e4efb231a8c004bcfd768e
slavkoegi@gmail.com <AnSa1991>
2020-07-13 17:22:23 +0500

    выделили методы

--- fresh-motka/auth/login-logout-python-wd.py
+++ fresh-motka/auth/login-logout-python-wd.py
@@ -2,42 +2,48 @@ import unittest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
-
 
 
 class LoginLogout(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.set_window_size(1280, 984)
         self.driver.implicitly_wait(30)
         self.verificationErrors = []
         self.accept_next_alert = True
 
+    def open_home_page(self, driver):
+        driver.get("http://www.motka-freshdoc.qds.ru/")
+
+    def find_button_login(self, driver):
+        driver.find_element_by_css_selector("li.qd__navigation__item > a.js-login.button-login").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").send_keys(username)
+        driver.find_element_by_name("password").click()
+        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_css_selector("button.button.expand.qd-overlay-auth__button").click()
+
+    def logout(self):
+        log_out = WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located((By.CSS_SELECTOR, "span.DD-button__caption")))
+        log_out.click()
+        log_out2 = WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located((By.CSS_SELECTOR, "span.DD-button__caption")))
+        log_out2.click()
+
     def test_login_logout(self):
         driver = self.driver
-        # Label: Open home page
-        driver.get("http://www.motka-freshdoc.qds.ru/")
-        # Label: Click buttons "Войти"
-        driver.find_element_by_css_selector("li.qd__navigation__item > a.js-login.button-login").click()
-        # Label: Test login
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").send_keys("test.qds.777@bk.ru")
-        driver.find_element_by_name("password").click()
-        driver.find_element_by_name("password").send_keys("AnSa1991")
-        driver.find_element_by_css_selector("button.button.expand.qd-overlay-auth__button").click()
-        # Label: Test Logout
-        login_out = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, "span.DD-button__caption")))
-        login_out.click()
-        login_out2 = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, "span.DD-button__caption")))
-        login_out2.click()
-
+        self.open_home_page(driver)
+        self.find_button_login(driver)
+        self.login(driver, username="test.qds.777@bk.ru", password="AnSa1991")
+        self.logout()
 
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
-
 
 if __name__ == "__main__":
     unittest.main()
--- fresh-motka/auth/test_opendoc.py
+++ fresh-motka/auth/test_opendoc.py
@@ -1,50 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 
 class TestOpendoc():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    self.wd = webdriver.Chrome()
     self.vars = {}
   
   def teardown_method(self, method):
-    self.driver.quit()
+    self.wd.quit()
   
   def test_opendoc(self):
-    self.driver.get("http://www.motka-freshdoc.qds.ru/")
-    self.driver.set_window_size(1280, 984)
-    self.driver.find_element(By.LINK_TEXT, "test.qds.777@bk.ru").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".qd-filelist-table-item:nth-child(4) .qd-filelist-table-item__field_\\418\\43C\\44F")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    element = self.driver.find_element(By.LINK_TEXT, "Мои шаблоны")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".qd-filelist-table-item:nth-child(3) .qd-filelist-table-item__field_\\418\\43C\\44F")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.CSS_SELECTOR, ".qd-filelist-table-item:nth-child(2) .qd-filelist-table-item__field_\\418\\43C\\44F").click()
-    element = self.driver.find_element(By.LINK_TEXT, "Договоры")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.LINK_TEXT, "Типовой договор купли-продажи").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".qd-carcass-btn-save").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".DD-group-flat:nth-child(3) .DD-button:nth-child(2)")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    self.driver.find_element(By.CSS_SELECTOR, ".qd-icon_close").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".qd-icon_home").click()
-  
+    self.wd.get("http://www.motka-freshdoc.qds.ru/")
+    self.wd.set_window_size(1280, 984)
+
+  def find_button_login(self, wd):
+    self.wd.find_element_by_css_selector("li.qd__navigation__item > a.js-login.button-login").click()
+
+  def login(self, wd, username, password):
+    self.wd.find_element_by_name("email").click()
+    self.wd.find_element_by_name("email").send_keys(username)
+    self.wd.find_element_by_name("password").click()
+    self.wd.find_element_by_name("password").send_keys(password)
+    self.wd.find_element_by_css_selector("button.button.expand.qd-overlay-auth__button").click()
+    self.wd.find_element(By.LINK_TEXT, "test.qds.777@bk.ru").click()
+    self.wd.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[2]/button").click()
+    time.sleep(5)
+    self.wd.find_element(By.LINK_TEXT, "Типовой договор купли - продажи").click()
+time.sleep(5)
