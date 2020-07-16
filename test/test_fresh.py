c78fa9d5c1c6a8e4d82cfcece619e2e46778afe3
slavkoegi@gmail.com <AnSa1991>
2020-07-16 10:55:36 +0500

    созданы пакеты для разных зон ответственности

---
+++ test/test_fresh.py
@@ -0,0 +1,18 @@
+import pytest
+from fixture.application import Application
+
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_login_logout(app):
+    app.open_home_page()
+    app.find_button_login()
+    app.login(username="test.qds.777@bk.ru", password="AnSa1991")
+    app.create_new_doc()
+    app.logout()
+
