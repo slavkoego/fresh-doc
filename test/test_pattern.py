6270026b70dd355d626ba0c717e7d288e1676d46
slavkoegi@gmail.com <AnSa1991>
2020-07-16 18:22:34 +0500

    переименовал тест на более очевидное имя

--- test/test_pattern.py
+++ test/test_pattern.py
@@ -1,6 +1,6 @@
 
 
-def test_pattern(app):
+def test_new_pattern(app):
     app.session.open_home_page()
     #app.session.login(username="test.qds.777@bk.ru", password="AnSa1991")
     app.pattern.create_new_pattern()
